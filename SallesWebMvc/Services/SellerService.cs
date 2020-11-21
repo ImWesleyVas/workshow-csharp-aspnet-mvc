bd8794f1df981dda5bf49251e603b86771073799
ImWesleyVas <souwesleyroque@gmail.com>
2020-11-21 12:12:51 -0300

    Update seller and custom service exception

--- SallesWebMvc/Services/SellerService.cs
+++ SallesWebMvc/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SallesWebMvc.Services.Exceptions;
 
 namespace SallesWebMvc.Services
 {
@@ -39,6 +40,24 @@ namespace SallesWebMvc.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update (Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new DllNotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
